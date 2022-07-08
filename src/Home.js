8a52c16208df30ddb90ab1ead0de5ee87151459a
meg151 <meg101810@gmail.com>
2022-07-08 15:07:08 +0900

    props **

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'oh yeah', author: 'yoda', id: 1 },
+    {
+      title: 'obiwan kenobi freakin cool',
+      body: 'last episode..',
+      author: 'ben',
+      id: 2,
+    },
+    { title: 'this is the way..', body: 'so cool', author: 'mando', id: 3 },
+  ]);
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} />
+    </div>
+  );
+};
+
+export default Home;
