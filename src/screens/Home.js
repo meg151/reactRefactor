5fdb0bc0bd480ebbc71c02e7b8ea83c0d2935202
beautymakersDev <beautymakersDev@gmail.com>
2022-07-15 22:44:35 +0900

    axios installed, instance created

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,7 @@
 import BlogList from 'components/BlogList/BlogList';
 import useFetch from 'hooks/useFetch';
+import { useQuery } from 'react-query';
+import { fetchBlog } from 'utils/api/blogsApi';
 
 const Home = () => {
   const {
@@ -7,6 +9,11 @@ const Home = () => {
     isPending,
     error,
   } = useFetch('http://localhost:8000/blogs');
+
+  // const { isLoading, isError, data, error } = useQuery(
+  //   'blogs',
+  //   () => fetchBlogs
+  // );
 
   return (
     <div className="home">
